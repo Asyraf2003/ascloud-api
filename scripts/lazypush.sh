82b8232f0655ef21713a0fb1e59cd55b6b4cd1c0
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-03-01 11:58:24 +0800

    Update 71

--- scripts/lazypush.sh
+++ scripts/lazypush.sh
@@ -2,7 +2,7 @@ count=$(($(git rev-list --count HEAD 2>/dev/null || echo 0) + 1))
 git add .
 if git commit -m "Update $count"; then
     echo "Berhasil commit: Update $count"
-    git push origin main
+    git push -u origin main
 else
     echo "Gagal: Tidak ada perubahan untuk di-commit atau terjadi error."
 fi
